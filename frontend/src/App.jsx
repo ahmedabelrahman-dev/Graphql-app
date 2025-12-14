ecbe021a8d1e9eeeb16991a1f0e326ed8e6b5189
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-14 21:38:51 +0400

    feat: handling client routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,46 @@
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import SignUpPage from './pages/SignUpPage.jsx';
-import TransactionPage from './pages/TransactionPage.jsx';
-import NotFound from './pages/NotFound.jsx';
-import Header from './components/layout/Header.jsx';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignupPage';
+import TransactionPage from './pages/TransactionPage';
+import NotFoundPage from './pages/NotFound.jsx';
+import Header from './components/layout/Header';
+import { useQuery } from '@apollo/client/react';
+import { GET_AUTHENTICATED_USER } from './graphql/queries/user.query';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  const authUser = true;
+  const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
+
+  if (loading) return null;
+
   return (
     <>
-      {authUser && <Header />}
+      {data?.authUser && <Header />}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/transaction/:id" element={<TransactionPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/"
+          element={data.authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!data.authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!data.authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/transaction/:id"
+          element={
+            data.authUser ? <TransactionPage /> : <Navigate to="/login" />
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      <Toaster />
     </>
   );
 }
+
 export default App;
